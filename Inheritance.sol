d7279067920794c963afe923cd125b1b136c9581
Karan Singh <singh_karan@hotmail.com>
2021-04-30 11:59:31 +0530

    inheritance constructor

--- Inheritance.sol
+++ Inheritance.sol
@@ -13,13 +13,26 @@ Examples:
 
 contract A {
 
+    string public name;
+
+    constructor(string memory _name) public{
+        name = _name;
+    }
+
     function getContractName() public pure returns (string memory) {
         return "Contract A";
     }
 }
 
+/*  When overwriting a contract make sure that the function signature is 
+    the same as the parent contract otherwise it will not overwite it. 
+*/ 
 contract B is A {
     
+    constructor(string memory _name) A (_name) public{
+
+    }
+
     function getContractName() public pure returns (string memory) {
         return "Contract B";
     }
