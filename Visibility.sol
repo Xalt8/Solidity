7d0f1e5c9922fd41d8cb0ee63355734d9fd530fc
Karan Singh <singh_karan@hotmail.com>
2021-05-04 16:17:47 +0530

    visibility second commit

--- Visibility.sol
+++ Visibility.sol
@@ -19,6 +19,29 @@ contract Base {
     function testInternalFunc() public pure returns (string memory) {
         return internalFunc();
     }
+
+    /*  public functions can be called inside this contract
+        by child contracts inheriting from this contract
+        by other contracts and accounts. 
+    */ 
+    function publicFunc() public pure returns (string memory) {
+        return "public function called";
+    }
+
+    // Can only be called by external contracts and accounts.
+    // Cannot call this function inside another function 
+    function externalFunc() external pure returns (string memory) {
+        return "external function called";
+    }
+
+    string private privateVar = "my private variable";
+    string internal internalVar = "my internal variable";
+    string public publicVar = "my public variable";
+
+    // State variables cannot be declared as external!
+    // string external externalVar = "my external variable";
+
+
 }
 
 contract Child is Base {
